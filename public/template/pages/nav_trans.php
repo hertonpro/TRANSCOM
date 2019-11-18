a523863578d816f0c321051e1c41b0d7fab623a5
hertonpro <hertonpro@users.noreply.github.com>
2019-11-18 13:40:11 +0200

    systheme de autoloader

---
+++ public/template/pages/nav_trans.php
@@ -0,0 +1,4 @@
+<div class="btn-group btn-group-sm" role="group" aria-label="...">
+    <button type="button" class="btn btn-secondary">1</button>
+    <button type="button" class="btn btn-secondary">2</button>
+</div>
