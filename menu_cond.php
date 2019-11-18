24772105804bc4bda08f4990c7c48d493bf6c3de
miambajeef <57457082+miambajeef@users.noreply.github.com>
2019-11-18 12:10:06 +0100

    Delete menu_cond.php

--- menu_cond.php
+++
@@ -1,2 +0,0 @@
-<a href="dashboard_operateur.php"><button class="btn-sm btn-primary">TABLEAU DE BORD</button></a>
-<a href="saisie_permis_cond.php"><button class="btn-sm btn-primary">Permis</button></a>
