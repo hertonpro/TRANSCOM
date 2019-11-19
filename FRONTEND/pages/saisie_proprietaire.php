5e13e67ae3696a2af78194ae384213cccb6905d7
miambajeef <57457082+miambajeef@users.noreply.github.com>
2019-11-19 12:02:03 +0100

    Add files via upload

---
+++ FRONTEND/pages/saisie_proprietaire.php
@@ -0,0 +1,77 @@
+<div class="row">
+    <div class="col-lg-6">
+        <div class="panel panel-default">
+            <div class="panel-heading">fiche d'identification</div>
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+                        <form role="form">
+                            <div class="form-group">Nom
+<input class="form-control" placeholder="Enter text">
+</div>
+                            <div class="form-group">Post-nom
+<input class="form-control" placeholder="Enter text">
+</div>
+                            <div class="form-group">
+                              <label>Prenom </label>
+                              <input class="form-control" placeholder="Enter text">
+                            </div>
+							<label>Sexe</label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="optionsRadiosInline" id="optionsRadiosInline1" value="option1" checked> homme
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="optionsRadiosInline" id="optionsRadiosInline2" value="option2">femme
+                                </label>
+							<div class="form-group">
+                                <label>Date de naissance</label>
+                                <input type="date" class="form-control" placeholder="Enter text">
+                            </div>
+							<div class="form-group">
+							  <label>Lieu de naissance</label>
+							  <input class="form-control" placeholder="Enter text">
+                            </div>
+							<div class="form-group">
+                            	
+                            </div>
+                            <div class="form-group"> Province
+                              <input class="form-control" placeholder="Enter text">
+                            </div>
+							<div class="form-group"> ville
+							  <input class="form-control" placeholder="Enter text">
+                          </div>
+							<div class="form-group">Commune&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+<input class="form-control" placeholder="Enter text">
+                          </div>
+							<div class="form-group"> Quartier
+							  <input class="form-control" placeholder="Enter text">
+                          </div>
+							<div class="form-group">Avennue&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+<input class="form-control" placeholder="Enter text">
+                          </div>
+						  <div class="form-group"> Numero domicile
+						    <input class="form-control" placeholder="Enter text">
+                            </div>
+							<div class="form-group">Phone&nbsp; &nbsp; &nbsp; &nbsp;
+<input class="form-control" placeholder="Enter text">
+                            </div>
+							<div class="form-group">Mail&nbsp; &nbsp; &nbsp; &nbsp;
+<input type="mail" class="form-control" placeholder="exemple(exemple@mail.com)">
+                          </div>
+                            <div class="form-group">
+                                    <label>Image</label>
+                                    <input class="form-control" type="file" placeholder="Enter text">
+                            </div>
+							<div class="form-group">
+                                    <label>Scan ID</label>
+                                    <input class="form-control" type="file" placeholder="Enter text">
+                            </div>
+                            
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary btn-positio-left">suivant</button>
+                            </div>
+                            <button type="reset" class="btn btn-default">Reset Button</button>
+                        </form>
+            </div>
+        </div>
+    </div>
+</div>                
